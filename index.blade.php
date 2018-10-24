8b59ea551c1d82f2d1046b9b719155bbebf2ad88
marco xot <marco.sottana@gmail.com>
2018-10-24 12:12:24 +0200

    conflitto strano

--- index.blade.php
+++ index.blade.php
@@ -2,6 +2,7 @@
 @section('content')
 @include('pub_theme::includes.components')
 @include('pub_theme::includes.flash')
+<<<<<<< Updated upstream
 <div id="page-body-inner">
 	{{--
 	it/forum_cat/create
@@ -21,3 +22,22 @@
 @include($view.'.sidebar')
 @endsection
 				
+=======
+	<div id="page-body-inner">
+		{{--
+		it/forum_cat/create
+		 it/{container}/create   => container.create    
+		--}}
+		@php
+			$url=route('container.create',array_merge($params,['container'=>'forum_cat']));
+		@endphp
+		<a href="{{ $url }}" class="btn btn-default"><i class="fa fa-plus"></i>&nbsp;Add Category</a>
+		@foreach($forum_cat->archive as $item)
+			@include('pub_theme::layouts.items.forum_cat',['row'=>$item])
+		@endforeach
+		@include('pub_theme::widgets.who_is_online')
+		@include('pub_theme::widgets.statistics')
+	</div>
+	@include($view.'.sidebar')
+@endsection
+>>>>>>> Stashed changes
