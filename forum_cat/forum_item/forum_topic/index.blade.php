fcdfd5716130b032f1f4513396c98e1fd2f4b467
marco xot <marco.sottana@gmail.com>
2018-10-30 00:16:49 +0100

    fix

--- forum_cat/forum_item/forum_topic/index.blade.php
+++ forum_cat/forum_item/forum_topic/index.blade.php
@@ -3,10 +3,10 @@
 @include('pub_theme::includes.components')
 @include('pub_theme::includes.flash') 
     <h2>Topics List</h2>
-    {{-- ddd($item01->title) --}}
+    {{-- ddd($item1->title) --}}
     <ul class="topiclist topics">
     <ul class="topiclist forums">
-    @foreach($item01->related('forum_topic')->paginate() as $item0)
+    @foreach($item1->related('forum_topic')->paginate() as $item0)
         @include('pub_theme::layouts.items.forum_topic',['row'=>$item0])
     @endforeach
     </ul>
