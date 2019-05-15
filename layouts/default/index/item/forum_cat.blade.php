07173fa076bc4de0eefa335687e42b2211ea299b
marco76tv <marco.sottana@gmail.com>
2019-05-15 13:42:27 +0200

    .

--- layouts/default/index/item/forum_cat.blade.php
+++ layouts/default/index/item/forum_cat.blade.php
@@ -10,12 +10,12 @@
 		</li>
 	</ul>
 	<ul class="topiclist forums">
-		{!! Form::bsBtnCreateRelated(['row'=>$row,'related'=>$forum_item->getLinkedModel()]) !!}
-		@can('create',$forum_item->getLinkedModel())
-		<li>
-			<a href="{{ route('container0.container1.create',['container0'=>'forum_cat','item0'=>$row,'container1'=>$forum_item->post_type]) }}" class="btn btn-secondary"><i class="fa fa-plus"></i>&nbsp;Add Item</a>
-		</li>
-		@endcan
+		{!! Form::bsBtnCreateRelated([
+			'txt'=>'Add Item',
+			'row'=>$row,
+			'related'=>$forum_item->getLinkedModel(),
+			]) !!}
+		
 
 		@foreach($row->forumItems()->paginate() as $item0)
 			@include('pub_theme::layouts.default.index.item.forum_item',['row'=>$item0])
